af428e444cfc36823b5a31ac710b777a0506bcec
GiseleAndrade17 <giselemariadeandrade@gmail.com>
2025-02-04 21:24:06 +0000

    código inicial javascript

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  const campo = document.getElementById("amigo");
+  const nome = campo.value.trim();
+
+  if (nome === "") {
+    alert("Por favor insira um nome.");
+    return;
+  }
+
+  amigos.push(nome);
+
+  campo.value = "";
+  console.log("Lista de amigos:", amigos);
+}
+
+function atualizarLista() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigos() {
+  if ((amigos.length = 0)) {
+    alert("Nenhum amigo disponível para ser sorteado.");
+    return;
+  }
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  document.getElementById("resultado").innerHTML =
+    "Amigo sorteado:" + amigoSorteado;
+}
